41b8d2bba454b8d23da7d094cc6f38cf7a63ee40
MagicTINTIN <magictintin@proton.me>
2024-11-11 23:11:53 +0100

    clean: removing unused class

--- src/main/java/com/JIRSend/db/TestDatabase.java
+++
@@ -1,8 +0,0 @@
-package com.JIRSend.db;
-
-public class TestDatabase extends ACLVDatabase {
-    // Test database
-    public TestDatabase() {
-        super("needs", "to", "be", "changed");
-    }
-}
